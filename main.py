de82f4f7ec4b94a279a53ef57686e697105e8a5f
Sanket More <sanketmore262002@gmail.com>
2024-04-10 12:55:57 +0530

    Ip Changes

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ def index():
     return {'message' : 'Hello World'}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
     
 #python -m uvicorn main:app --reload
